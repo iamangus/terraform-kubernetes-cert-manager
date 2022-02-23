83204dda72f4b6ce422cf1b5a7aec0d30c7a939e
Angus Turnbull <turnbull.angus@gmail.com>
2022-02-23 10:27:59 -0600

    v3

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 0.14.8, < 2.0.0"
-  experiments      = ["module_variable_optional_attrs"]
+  experiments      = [module_variable_optional_attrs]
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
