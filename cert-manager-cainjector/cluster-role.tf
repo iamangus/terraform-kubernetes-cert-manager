35e7831ba15ba52b86166e86caff4c13a9351cfb
iamangus <turnbull.angus@gmail.com>
2022-02-23 19:17:59 -0600

    Update cluster-role.tf

--- cert-manager-cainjector/cluster-role.tf
+++ cert-manager-cainjector/cluster-role.tf
@@ -29,6 +29,14 @@ resource "kubernetes_cluster_role" "cluster_role" {
     ]
     verbs = ["get", "create", "update", "patch"]
   }
+  
+  rule {
+    api_groups = [""]
+    resources = [
+      "configmaps"
+    ]
+    verbs = ["get", "create", "update"]
+  }
 
   rule {
     api_groups = ["admissionregistration.k8s.io"]
@@ -45,6 +53,14 @@ resource "kubernetes_cluster_role" "cluster_role" {
       "apiservices"
     ]
     verbs = ["get", "list", "watch", "update"]
+  }
+  
+  rule {
+    api_groups = ["coordination.k8s.io"]
+    resources = [
+      "leases"
+    ]
+    verbs = ["create", "get", "update"]
   }
 
   rule {
